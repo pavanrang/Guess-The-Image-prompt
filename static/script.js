3275902e21f91921fb2ecc60e2b68fc8ab382165
Satya viswa pavan Ranga <pavan.sv.ranga@gmail.com>
2024-05-26 19:32:12 -0400

    all working except original prompt

--- static/script.js
+++ static/script.js
@@ -17,17 +17,25 @@ $(document).ready(function() {
     });
 
     $('#submitGuessBtn').click(function() {
-        const originalPrompt = $('#hintsList').text();
+        const originalPromptText = $('#hintsList').text().replace('Hints: ', '');
         const guessedPrompt = $('#guessedPrompt').val();
 
         $.ajax({
             url: 'http://127.0.0.1:8000/get-score',
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify({ originalPrompt, guessedPrompt }),
+            data: JSON.stringify({ originalPrompt: originalPromptText, guessedPrompt }),
             success: function(response) {
                 $('#similarityScore').text(response.score);
                 $('#guessedImage').attr('src', response.generatedImage);
+                
+                // Show original prompt below the generated image
+                $('#originalPrompt').text('Original Prompt: ' + originalPromptText);
+                $('#originalPrompt').removeClass('hidden');
+                
+                // Show guessed prompt below the guessed image
+                $('#guessedPromptText').text('Guessed Prompt: ' + guessedPrompt);
+
                 $('#hintGuessContainer').replaceWith($('#resultContainer')); // Replace hint-guess-container with resultContainer
                 $('#resultContainer').removeClass('hidden');
             },
